bb470b8942092e08eee256839eb18fb64e5fa4d0
Gautam Mishra <mishra.gautam96@gmail.com>
2017-12-16 22:08:21 +0000

    style show page campgrounds

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
+app.use(express.static(__dirname + "/public"));
 
 // root route
 app.get("/", function(req, res){
--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
             {
             name: "Cloud's Rest", 
             image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
-            description: "blah blah blah"
+            description: "Pellentesque luctus sollicitudin eros nec faucibus. Quisque sed tortor metus. Vivamus facilisis ultricies laoreet. Etiam quis ipsum nec quam varius malesuada. Aliquam non lorem a arcu interdum lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed euismod nec urna vel semper. Ut malesuada ornare erat, a placerat magna ultrices sed. Curabitur volutpat orci quis nisl pretium facilisis. Mauris leo diam, finibus non eros nec, ullamcorper congue eros. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
         },
         {
             name: "Desert Mesa", 
             image: "https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg",
-            description: "blah blah blah"
+            description: "Pellentesque luctus sollicitudin eros nec faucibus. Quisque sed tortor metus. Vivamus facilisis ultricies laoreet. Etiam quis ipsum nec quam varius malesuada. Aliquam non lorem a arcu interdum lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed euismod nec urna vel semper. Ut malesuada ornare erat, a placerat magna ultrices sed. Curabitur volutpat orci quis nisl pretium facilisis. Mauris leo diam, finibus non eros nec, ullamcorper congue eros. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
         },
         {
             name: "Canyon Floor", 
             image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
-            description: "blah blah blah"
+            description: "Pellentesque luctus sollicitudin eros nec faucibus. Quisque sed tortor metus. Vivamus facilisis ultricies laoreet. Etiam quis ipsum nec quam varius malesuada. Aliquam non lorem a arcu interdum lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed euismod nec urna vel semper. Ut malesuada ornare erat, a placerat magna ultrices sed. Curabitur volutpat orci quis nisl pretium facilisis. Mauris leo diam, finibus non eros nec, ullamcorper congue eros. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
         }
     ]
 
