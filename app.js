579af2f02123fa2d0486ed86df945510e1d11e10
Gautam Mishra <mishra.gautam96@gmail.com>
2017-12-16 18:00:55 +0000

    add campground module

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose");
+    mongoose    = require("mongoose"),
+    Campground  =  require("./models/campground");
 
 mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgroundSchema = mongoose.Schema({
-    name:String,
-    image:String,
-    description:String
-});
 
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create({
 //             name: "Dudhni, Silvassa", 
---
+++ models/campground.js
@@ -0,0 +1,8 @@
+var mongoose    = require("mongoose");
+var campgroundSchema = mongoose.Schema({
+    name:String,
+    image:String,
+    description:String
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
