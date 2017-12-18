03b18f87a28173909e661d560bc4a6ef73c6f445
Gautam Mishra <mishra.gautam96@gmail.com>
2017-12-18 19:35:58 +0000

    add features delete and update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express                 =   require("express"),
     mongoose                =   require("mongoose"),
     passport                =   require("passport"),
     LocalStrategy           =   require("passport-local"),
+    methodOverride          =   require("method-override"),
     passportLocalMongoose   =   require("passport-local-mongoose"),
     Campground              =   require("./models/campground"),
     Comment                 =   require("./models/comment"),
@@ -19,6 +20,7 @@ mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 
 //Passport Setup
 app.use(require("express-session")({
@@ -41,6 +43,7 @@ app.use(function(req, res, next){
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server has started!!");
